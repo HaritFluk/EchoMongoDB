14b3fcd996f76e3acefbf0f9e5e9d8de78bf28fa
DekuKung <07ghostharitkumsan@gmail.com>
2022-01-20 20:33:41 +0700

    second commit

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -18,6 +18,7 @@ import (
 var userCollections *mongo.Collection = configs.GetCollection(configs.DB, "users")
 var validate = validator.New()
 
+// CreateUser
 func CreateUser(c echo.Context) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	var user models.User
@@ -47,6 +48,7 @@ func CreateUser(c echo.Context) error {
 	return c.JSON(http.StatusCreated, responses.UserResponse{Status: http.StatusCreated, Message: "success", Data: &echo.Map{"data": result}})
 }
 
+// Get User 
 func GetAUser(c echo.Context) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	userId := c.Param("userId")
@@ -61,4 +63,89 @@ func GetAUser(c echo.Context) error {
 		return c.JSON(http.StatusInternalServerError, responses.UserResponse{Status: http.StatusInternalServerError, Message: "error", Data: &echo.Map{"data": err.Error()}})
 	}
 	return c.JSON(http.StatusOK, responses.UserResponse{Status: http.StatusOK, Message: "success", Data: &echo.Map{"data": user}})
+}
+
+// Edit User
+func EditAUser(c echo.Context) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	userId := c.Param("userId")
+	var user models.User
+	defer cancel()
+
+	objId, _ := primitive.ObjectIDFromHex(userId)
+
+	// validate the request body
+	if err := c.Bind(&user); err != nil {
+		return c.JSON(http.StatusBadRequest, responses.UserResponse{Status: http.StatusBadRequest, Message: "error", Data: &echo.Map{"data": err.Error()}})
+	}
+
+	// use the validator to validate required fields
+	if validationErr := validate.Struct(&user); validationErr != nil {
+		return c.JSON(http.StatusBadRequest, responses.UserResponse{Status: http.StatusBadRequest, Message: "error", Data: &echo.Map{"data": validationErr.Error()}})
+	}
+
+	update := bson.M{"name": user.Name, "location": user.Location, "title": user.Title}
+	
+	result, err := userCollections.UpdateOne(ctx, bson.M{"id": objId}, bson.M{"$set": update})
+
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, responses.UserResponse{Status: http.StatusInternalServerError, Message: "error", Data: &echo.Map{"data": err.Error()}})
+	}
+
+	// get updated user details
+	var updatedUser models.User
+	if result.MatchedCount == 1 {
+		err := userCollections.FindOne(ctx, bson.M{"id": objId}).Decode(&updatedUser)
+
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, responses.UserResponse{Status: http.StatusInternalServerError, Message: "error", Data: &echo.Map{"data": err.Error()}})
+	}
+}
+return c.JSON(http.StatusOK, responses.UserResponse{Status: http.StatusOK, Message: "success", Data: &echo.Map{"data": updatedUser}})
+}
+
+// Delete User
+func DeleteAUser(c echo.Context) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	userId := c.Param("userId")
+	defer cancel()
+
+	objId, _ := primitive.ObjectIDFromHex(userId)
+
+	result, err := userCollections.DeleteOne(ctx, bson.M{"userId": objId})
+
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, responses.UserResponse{Status: http.StatusInternalServerError, Message: "error", Data: &echo.Map{"data": err.Error()}})
+	}
+
+	if result.DeletedCount < 1 {
+		return c.JSON(http.StatusNotFound, responses.UserResponse{Status: http.StatusNotFound, Message: "error", Data: &echo.Map{"data": "User with specified ID not found!"}})
+	}
+	return c.JSON(http.StatusOK, responses.UserResponse{Status: http.StatusOK, Message: "success", Data: &echo.Map{"data": "User successfully deleted!"}})
+}
+
+// Get All Users
+func GetAllUsers(c echo.Context) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	var users []models.User
+	defer cancel()
+
+	result, err := userCollections.Find(ctx, bson.M{})
+
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, responses.UserResponse{Status: http.StatusInternalServerError, Message: "error", Data: &echo.Map{"data": err.Error()}})
+	}
+
+	// reading from the bd in an optimal way
+	defer result.Close(ctx)
+	for result.Next(ctx) {
+		var singleUser models.User
+		if err = result.Decode(&singleUser); err != nil {
+			return c.JSON(http.StatusInternalServerError, responses.UserResponse{Status: http.StatusInternalServerError, Message: "error", Data: &echo.Map{"data": err.Error()}})
+		}
+
+		users = append(users, singleUser)
+	}
+
+	return c.JSON(http.StatusOK, responses.UserResponse{Status: http.StatusOK, Message: "success", Data: &echo.Map{"data": users}})
 }
--- routes/user_route.go
+++ routes/user_route.go
@@ -9,9 +9,14 @@ import (
 func UserRoute(e *echo.Echo) {
 	//All Routes related to user come here
 
-	// POST /users
+	// Create User
 	e.POST("/user", controllers.CreateUser)
-
-	// GET /users
-	e.GET("/users", controllers.GetAUser)
+	// GET User
+	e.GET("/user", controllers.GetAUser)
+	// Edit User
+	e.PUT("/user/:userId", controllers.EditAUser)
+	// Delete User
+	e.DELETE("/user/:userId", controllers.DeleteAUser)
+	// Get All Users
+	e.GET("/users", controllers.GetAllUsers)
 }
